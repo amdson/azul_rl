544fb895ffef5a5e0f56120dd4d5cb0028c6d32c
Engineering Student Council at Berkeley <ryanhuntley23@berkeley.edu>
2022-11-21 14:42:39 -0800

    Initial Simulator API

---
+++ Azul_Simulator.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 21 14:23:02 2022
+
+@author: Ryan
+"""
+
+def take_action(state, action, rand):
+    """
+    Parameters
+    ----------
+    state : vector
+        DESCRIPTION.
+    action : int
+        DESCRIPTION.
+    rand : numpy seed
+        DESCRIPTION.
+
+    Returns
+    -------
+    state : vector
+        DESCRIPTION.
+    reward : int
+        DESCRIPTION.
+    """
+    
+def init_state(rand):
+    """
+    Parameters
+    ----------
+    rand : numpy seed
+        DESCRIPTION.
+    
+    Returns
+    -------
+    state : vector
+        DESCRIPTION.
+    """
+    
+def flip_state(state):
+    """
+    Parameters
+    ----------
+    state : vector
+        DESCRIPTION.
+
+    Returns
+    -------
+    state : vector
+        DESCRIPTION.
+    """    
+    
